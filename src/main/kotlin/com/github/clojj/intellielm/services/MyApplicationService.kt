3130ea003cf12fdf2007f1fd7ca12cf7082bbf71
GitHub Action <action@github.com>
2022-02-06 13:21:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/clojj/intellielm/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/clojj/intellielm/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.clojj.intellielm.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.clojj.intellielm.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/clojj/intellielm/services/MyProjectService.kt
+++ src/main/kotlin/com/github/clojj/intellielm/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.clojj.intellielm.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.clojj.intellielm.MyBundle
 
 class MyProjectService(project: Project) {
 
